c9c51c9b66c5803719f097d7c6bbc67dd90a6b68
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-02 18:03:15 -0300

    feat: micro-cardapio rodando com React e Webpack

--- micro-cardapio/webpack.config.js
+++ micro-cardapio/webpack.config.js
@@ -11,7 +11,12 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
     ],
   },
