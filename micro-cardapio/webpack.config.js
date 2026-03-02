9a35e0a9fe4acb63f3c272ffbc671086489c5055
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-02 16:40:29 -0300

    chore: setup inicial do projeto

---
+++ micro-cardapio/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
