e84803261bb57a8f41d9fb792b14f75e61fa25cf
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-03 18:32:45 -0300

    style: estiliza header

---
+++ container/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Eccellenza nel Sapore. Todos os direitos reservados.</p>
+      <p>Criado e desenvolvido por Felipe Augusto</p>
+    </footer>
+  );
+}
---
+++ container/src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section>
+      <img src="/image/hero_1536p.webp" alt="Hero" />
+    </section>
+  );
+}
