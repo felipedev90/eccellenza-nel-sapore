16f54987b51cfa856848be3fd0ea20e7eba67ca0
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-03 20:01:39 -0300

    fix: Ajusta dados do footer

--- container/src/components/Footer.js
+++ container/src/components/Footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-4 mt-5">
       <p className="mb-1">
-        © 2024 Eccellenza nel Sapore. Todos os direitos reservados.
+        © 2026 Eccellenza nel Sapore. Todos os direitos reservados.
       </p>
       <p className="mb-0">Criado e desenvolvido por Felipe Augusto</p>
     </footer>
