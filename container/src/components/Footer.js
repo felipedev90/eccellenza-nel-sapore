9c3b787cf8feebe9d217f7b1802190a1ef5f2c95
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-03 19:00:28 -0300

    style: footer

--- container/src/components/Footer.js
+++ container/src/components/Footer.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer>
-      <p>© 2024 Eccellenza nel Sapore. Todos os direitos reservados.</p>
-      <p>Criado e desenvolvido por Felipe Augusto</p>
+    <footer className="bg-dark text-white text-center py-4 mt-5">
+      <p className="mb-1">
+        © 2024 Eccellenza nel Sapore. Todos os direitos reservados.
+      </p>
+      <p className="mb-0">Criado e desenvolvido por Felipe Augusto</p>
     </footer>
   );
 }
