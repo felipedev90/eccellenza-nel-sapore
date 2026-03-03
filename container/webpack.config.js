0a9e2db52cfac09a37817fae2a4e3db8c2e021b6
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-03 15:41:06 -0300

    feat: cria e configura setup base do container

---
+++ container/webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
