4cf321c300d7691c1e8ea95bc15b730536c3432d
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-03 18:53:41 -0300

    style: estiliza cardapio cards

--- container/src/components/Hero.js
+++ container/src/components/Hero.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <section className="position-relative">
+    <section className="position-relative overflow-hidden">
       <img
         src="/image/hero_1536p.webp"
         alt="Hero"
         className="img-fluid w-100"
       />
-      <div className="position-absolute top-50 start-50 translate-middle text-center text-white bg-dark bg-opacity-50 p-4 rounded">
+      <div className="position-absolute top-50 start-50 translate-middle text-center text-white bg-dark bg-opacity-50 p-4 rounded w-75">
         <p
           style={{
             fontFamily: "'Playfair Display', serif",
